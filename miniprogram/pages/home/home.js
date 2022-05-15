d2e5a27e3d4c60d0649d67313232cb9fc2739c08
CoderJoy <296351685@qq.com>
2022-05-16 01:01:50 +0800

    add banner and search in home page

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,11 +1,39 @@
+import Toast from '@vant/weapp/toast/toast';
 
 Page({
-  data: {},
+  data: {
+    value: "",
+    banner_url: [{
+        "id": 0,
+        "url": "/images/茶杯-小.jpeg"
+      },
+      {
+        "id": 1,
+        "url": "/images/运动女孩.jpeg"
+      }
+    ],
+    open: false,
+    indicatorDots: true, //是否显示面板指示点 
+    autoplay: true, //是否开启自动切换 
+    interval: 4000, //自动切换时间间隔 
+    duration: 500 //滑动动画时长
+  },
   onShow: function () {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         active: 'home'
       })
     }
+  },
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
+  onSearch() {
+    Toast('搜索' + this.data.value);
+  },
+  onClick() {
+    Toast('搜索' + this.data.value);
   }
 });
