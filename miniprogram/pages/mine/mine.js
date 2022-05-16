f67cbd82d2771d25d85a0e8812bb2d028dce3747
CoderJoy <296351685@qq.com>
2022-05-17 01:59:37 +0800

    add dropdown menu

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -1,70 +1,53 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    option1: [{
+        text: '全部活动',
+        value: 0
+      },
+      {
+        text: '我创建的',
+        value: 1
+      },
+      {
+        text: '我参与的',
+        value: 2
+      },
+    ],
+    option2: [{
+        text: '默认排序',
+        value: 'a'
+      },
+      {
+        text: '开始时间',
+        value: 'b'
+      },
+      {
+        text: '参与人数',
+        value: 'c'
+      },
+    ],
+    value1: 0,
+    value2: 'a',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  onOpen() {
+    console.log("open menu");
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  onFirstChange(value) {
+    console.log(value);
+    this.setData({
+      value1: value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  onSecondChange(detail) {
+    this.setData({
+      value2: detail
+    })
+  },
   onShow: function () {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         active: 'mine'
       })
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
   }
 })
-
