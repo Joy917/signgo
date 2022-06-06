302868ed66cb7e5272c2c9a22b7cd932662fb5ab
CoderJoy <296351685@qq.com>
2022-06-07 01:04:33 +0800

    add tecent map plugin

--- miniprogram/pages/create/create.js
+++ miniprogram/pages/create/create.js
@@ -1,8 +1,19 @@
+const chooseLocation = requirePlugin('chooseLocation');
+
+const key = '7WZBZ-3F3C3-RXA3C-3Z45A-EXNP2-2GBO7'; //使用在腾讯位置服务申请的key
+const referer = '活动报名冲'; //调用插件的app的名称
+let location = JSON.stringify({
+  latitude: 39.89631551,
+  longitude: 116.323459711
+});
+const category = '生活服务,休闲娱乐,运动健身';
+
 Page({
   data: {
     nameValue: '',
     typeValue: '点击选择',
     introValue: '',
+    siteValue: '',
     introSizeLimit: {
       maxHeight: 100,
       minHeight: 50
@@ -59,6 +70,11 @@ Page({
       introValue: event.detail
     });
   },
+  onSiteChange(event) {
+    this.setData({
+      siteValue: event.detail
+    });
+  },
   onClickType() {
     let typeDirect = 'down'
     if (this.data.typeArrowDirection === 'down') {
@@ -88,6 +104,13 @@ Page({
     });
     this.onClickType();
   },
+  onClickMap() {
+    if (location) {
+      wx.navigateTo({
+        url: `plugin://chooseLocation/index?key=${key}&referer=${referer}&location=${location}&category=${category}`
+      });
+    }
+  },
   afterRead(event) {
     const {
       file
@@ -116,6 +139,7 @@ Page({
     });
   },
   onShow: function () {
+    location = chooseLocation.getLocation(); // 如果点击确认选点按钮，则返回选点结果对象，否则返回null
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         active: 'create',
